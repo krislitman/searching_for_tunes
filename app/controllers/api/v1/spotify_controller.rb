c83ef4b28537fe7a3ba2476fe80892da802292fa
Kris Litman <kris.d.litman@gmail.com>
2021-07-14 14:56:24 -0400

    Added pagination for Album objects, only by page and will always return 20 objects

--- app/controllers/api/v1/spotify_controller.rb
+++ app/controllers/api/v1/spotify_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SpotifyController < ApplicationController
 
   def new_releases
-    spotify = SpotifyFacade.new(:new_releases)
+    spotify = SpotifyFacade.new({type: :new_releases, page: params[:page] ? params[:page] : 1})
     render json: AlbumSerializer.new(spotify.new_releases), status: :ok
   end
 end
--- spec/requests/get_new_releases_spec.rb
+++ spec/requests/get_new_releases_spec.rb
@@ -17,5 +17,33 @@ RSpec.describe "/new_releases", type: :request do
         expect(attributes[:data].length).to eq(20)
       end
     end
+
+    it "Should return another 20 albums for page 2" do
+      VCR.use_cassette("requests/new_releases_2") do
+        get '/api/v1/new_releases'
+
+        attributes_one = JSON.parse(response.body, symbolize_names: true)
+
+        attributes = {'page': 2}
+        get '/api/v1/new_releases', params: attributes
+
+        attributes_two = JSON.parse(response.body, symbolize_names: true)
+
+        expect(attributes_one[:data].length).to eq(20)
+        expect(attributes_two[:data].length).to eq(20)
+        expect(attributes_one[:data][0][:attributes][:name]).not_to eq(attributes_two[:data][0][:attributes][:name])
+      end
+    end
+
+    it "Page 3 should return only 10 objects" do
+      VCR.use_cassette("requests/new_releases_3") do
+        attributes = {'page': 3}
+        get '/api/v1/new_releases', params: attributes
+
+        attributes = JSON.parse(response.body, symbolize_names: true)
+
+        expect(attributes[:data].length).to eq(10)
+      end
+    end
   end
 end
